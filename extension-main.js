2e9f0e93efad3300220dd9dc99c084a571af6ea2
Martin Georgiev <martin@devision.bg>
2017-08-23 13:39:53 +0300

    first improvements and working functionality

--- extension-main.js
+++ extension-main.js
@@ -9,4 +9,25 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
+
+  var toggleCollapse = document.getElementById('btn-toggle-files-collapse');
+  toggleCollapse.addEventListener('click', function (event) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      console.log(tabs);
+      chrome.tabs.sendMessage(tabs[0].id, {action: "toggle-collapse"}, function(response) {
+        //
+      });
+    });
+  });
+
+  var toggleWideMode = document.getElementById('btn-toggle-wide-mode');
+  toggleWideMode.addEventListener('click', function (event) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      console.log(tabs);
+      chrome.tabs.sendMessage(tabs[0].id, {action: "toggle-wide-mode"}, function(response) {
+        //
+      });
+    });
+  });
+
 });
