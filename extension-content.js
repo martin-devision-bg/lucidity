820fc8e03da55ecb509b49c45c0cd06169c97d4f
Martin Georgiev <martin@devision.bg>
2017-08-25 12:14:55 +0300

    implemented raw stage of filtering of files

--- extension-content.js
+++ extension-content.js
@@ -53,17 +53,31 @@ let actionListPrFiles = () => {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(request, sender);
+    // console.log(request, sender);
 
     if (request.action === 'list-pr-files') {
       actionListPrFiles();
+    }
+
+    if (request.action === 'filter-files') {
+      let allFiles = document.querySelectorAll('div.file');
+      allFiles.forEach(function (file) {
+        removeClass(file, 'hidden');
+      });
+
+      if (request.filterValue.length > 0) {
+        let files = document.querySelectorAll('div.file div.file-info a:not([title*="' + request.filterValue + '"])');
+        files.forEach(function (file) {
+          addClass(file.parentElement.parentElement.parentElement, 'hidden');
+        });
+      }
     }
 
 
     if (request.action === 'toggle-collapse') {
       var fileHeaders = document.querySelectorAll('div.file-header button.js-details-target');
       fileHeaders.forEach(function (element) {
-        element.click()
+        element.click();
       });
     }
 
@@ -71,7 +85,7 @@ chrome.runtime.onMessage.addListener(
     if (request.action === 'collapse-files') {
       var fileHeaders = document.querySelectorAll('div.file:not(.open) div.file-header button.js-details-target');
       fileHeaders.forEach(function (element) {
-        element.click()
+        element.click();
       });
     }
 
@@ -79,7 +93,7 @@ chrome.runtime.onMessage.addListener(
     if (request.action === 'expand-files') {
       var fileHeaders = document.querySelectorAll('div.file.open div.file-header button.js-details-target');
       fileHeaders.forEach(function (element) {
-        element.click()
+        element.click();
       });
     }
 
@@ -101,3 +115,32 @@ chrome.runtime.onMessage.addListener(
 
   }
 );
+
+function toggleClass (element, className) {
+  let originalClass = element.getAttribute('class');
+  let wideModeClassPosition = originalClass.indexOf(className);
+
+  if (wideModeClassPosition > -1) {
+    originalClass = originalClass.replace(new RegExp(className, 'g'), '');
+    element.setAttribute('class', originalClass);
+  } else {
+    originalClass += ' ' + className;
+    element.setAttribute('class', originalClass);
+  }
+}
+
+function addClass (element, className) {
+  let originalClass = element.getAttribute('class');
+  originalClass += ' ' + className;
+  element.setAttribute('class', originalClass);
+}
+
+function removeClass (element, className) {
+  let originalClass = element.getAttribute('class');
+  let wideModeClassPosition = originalClass.indexOf(className);
+
+  if (wideModeClassPosition > -1) {
+    originalClass = originalClass.replace(new RegExp(className, 'g'), '');
+    element.setAttribute('class', originalClass);
+  }
+}
--- extension-main.js
+++ extension-main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var listPrFiles = document.getElementById('btn-list-pr-files');
   listPrFiles.addEventListener('click', function (event) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      console.log(tabs);
+      // console.log(tabs);
       chrome.tabs.sendMessage(tabs[0].id, {action: "list-pr-files"}, function(response) {
         //
       });
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var toggleCollapse = document.getElementById('btn-toggle-files-collapse');
   toggleCollapse.addEventListener('click', function (event) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      console.log(tabs);
+      // console.log(tabs);
       chrome.tabs.sendMessage(tabs[0].id, {action: "toggle-collapse"}, function(response) {
         //
       });
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var toggleWideMode = document.getElementById('btn-toggle-wide-mode');
   toggleWideMode.addEventListener('click', function (event) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      console.log(tabs);
+      // console.log(tabs);
       chrome.tabs.sendMessage(tabs[0].id, {action: "toggle-wide-mode"}, function(response) {
         //
       });
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var expandFiles = document.getElementById('btn-expand-files');
   expandFiles.addEventListener('click', function (event) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      console.log(tabs);
+      // console.log(tabs);
       chrome.tabs.sendMessage(tabs[0].id, {action: "expand-files"}, function(response) {
         //
       });
@@ -43,8 +43,18 @@ document.addEventListener('DOMContentLoaded', function () {
   var collapseFiles = document.getElementById('btn-collapse-files');
   collapseFiles.addEventListener('click', function (event) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      console.log(tabs);
+      // console.log(tabs);
       chrome.tabs.sendMessage(tabs[0].id, {action: "collapse-files"}, function(response) {
+        //
+      });
+    });
+  });
+
+  var filesFilter = document.getElementById('files-filter');
+  filesFilter.addEventListener('keyup', function (event) {
+    let filterValue = filesFilter.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "filter-files", filterValue: filterValue}, function(response) {
         //
       });
     });
